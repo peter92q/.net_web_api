28200c9d4dca14b88026229cc99b61ef87497635
Peter Petrov <exeption11111@gmail.com>
2023-02-27 17:50:00 +0200

    .NET WEB Api

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;  
+        }
+        //get request for all products
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+
+        }
+        //get request for single product
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
